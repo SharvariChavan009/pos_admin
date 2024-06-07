de59d9e112cf3d3dc81a4cda3e1eaa8ce06926ae
Digvijay Patil <digvijaypatil0911@gmail.com>
2024-06-07 12:21:59 -0700

    new design

--- lib/features/splash/presentation/splash_page.dart
+++ lib/features/splash/presentation/splash_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pos_admin/core/common/colors.dart';
 import 'package:pos_admin/core/routes/router.dart';
 import '../../../core/common/images/images_constant.dart';
-import '../../../core/utils/device_dimension.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,21 +16,31 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     // Navigate to login screen after 3 seconds
-    Future.delayed(Duration(seconds: 3), () {
-     router.go('/login');
+    Future.delayed(const Duration(seconds: 3), () {
+      router.go('/login');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Image.asset(
-        height: DeviceUtils.getDeviceDimension(context).height,
-        width: DeviceUtils.getDeviceDimension(context).width,
-        AppImages.splashImage,
-        fit: BoxFit.fill,
+      backgroundColor: AppColors.darkColor,
+      body: Center(
+        child: SizedBox(
+          width: 400.0,
+          child: TextLiquidFill(
+            text: 'QuickDine',
+            waveColor: Colors.blueAccent,
+            boxBackgroundColor: AppColors.darkColor,
+            textStyle: const TextStyle(
+              fontSize: 60.0,
+              fontWeight: FontWeight.bold,
+            ),
+            boxHeight: 300.0,
+          ),
+        ),
       ),
     );
   }
 }
+
